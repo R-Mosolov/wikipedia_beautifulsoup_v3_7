eb298b69e761b1e2629606efdf3f12dcf3915879
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-13 04:14:48 +0300

    #10@1h10m: add conditions series for transform month text to digit

--- modules/life_dates_parser/age_constructor.py
+++ modules/life_dates_parser/age_constructor.py
@@ -8,6 +8,7 @@ not_ages = duration_handler.run()[2]
 
 ages_in_brackets__only_ages = []
 not_full_ages__only_day = []
+not_full_ages__only_month = []
 not_full_ages__only_year = []
 
 
@@ -18,11 +19,37 @@ for age in ages_in_brackets:
         pass
     ages_in_brackets__only_ages.append(only_age)
 
-
 for day in not_full_ages:
     only_day = re.findall('\d\d', day)[0]
     not_full_ages__only_day.append(only_day)
 
+for month in not_full_ages:
+    current_month = month.split()[1]
+
+    if current_month == 'января':
+        not_full_ages__only_month.append('01')
+    if current_month == 'февраля':
+        not_full_ages__only_month.append('02')
+    if current_month == 'марта':
+        not_full_ages__only_month.append('03')
+    if current_month == 'апреля':
+        not_full_ages__only_month.append('04')
+    if current_month == 'мая':
+        not_full_ages__only_month.append('05')
+    if current_month == 'июня':
+        not_full_ages__only_month.append('06')
+    if current_month == 'июля':
+        not_full_ages__only_month.append('07')
+    if current_month == 'августа':
+        not_full_ages__only_month.append('08')
+    if current_month == 'сентября':
+        not_full_ages__only_month.append('09')
+    if current_month == 'октября':
+        not_full_ages__only_month.append('10')
+    if current_month == 'ноября':
+        not_full_ages__only_month.append('11')
+    if current_month == 'декабря':
+        not_full_ages__only_month.append('12')
 
 for age in not_full_ages:
     only_year = re.findall('\d\d\d\d', age)[0]
@@ -31,4 +58,5 @@ for age in not_full_ages:
 
 print('Массив возрастов: \n' + str(ages_in_brackets__only_ages))
 print('Массив дней рождения: \n' + str(not_full_ages__only_day))
+print('Массив месяцев рождения: \n' + str(not_full_ages__only_month))
 print('Массив годов рождения: \n' + str(not_full_ages__only_year))
