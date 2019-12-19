4c61f8ba2aa72af0d8ad5297d5d2916f03901a1b
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-19 22:03:04 +0300

    #21@0h52m: write over 33K scientists links in .txt file

--- modules/scientists_pages/modules/full_links_constructor.py
+++ modules/scientists_pages/modules/full_links_constructor.py
@@ -1,23 +1,18 @@
 from modules.scientists_pages.modules.data import scientists_pages
-from openpyxl import Workbook
-
-wb = Workbook()
-ws = wb.active
 
 
 def run():
     links_arr = scientists_pages.show()
+
     full_links = []
+    scientists_full_links = open('modules/data/scientists_full_links.txt', 'a')
 
     for link in links_arr:
         if link[0] == '/':
             link = 'https://ru.wikipedia.org' + link
             full_links.append(link)
-
-            # recording the result in an excel file
-            ws['A' + str(len(full_links))] = str(link) + ', '
-            wb.save("modules/data/scientists_full_links.xlsx")
-
             print(link)
+
+    scientists_full_links.write(str(full_links))
 
     return full_links
