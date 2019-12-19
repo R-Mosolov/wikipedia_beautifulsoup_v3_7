7e2902701d07218e985ea29c5cca87cd3cfad117
R-Mosolov <r.v.mosolov@ya.ru>
2019-12-19 23:26:54 +0300

    #22@1h25m: prepare parser to parsing unsorted ages for night

--- modules/life_dates/modules/ages_parser.py
+++ modules/life_dates/modules/ages_parser.py
@@ -6,19 +6,24 @@ from modules import session_generator
 def run():
     request = session_generator.run('https://ru.wikipedia.org/wiki/Категория:Социологи_по_алфавиту')
     links_arr = full_links_constructor.run()
+
     ages_arr = []
+    unsorted_ages = open('modules/data/unsorted_ages.txt', 'a')
 
     if request.status_code == 200:
+        unsorted_ages.truncate(0)
+
         for link in links_arr:
             request = session_generator.run(link)
             soup = bs(request.content, 'lxml')
 
             try:
-                age = soup\
-                    .find_all('span', attrs={'class', 'nowrap'})[0]\
+                age = soup \
+                    .find('span', attrs={'class', 'nowrap'}) \
                     .text
+                unsorted_ages.write(str("'" + age + "'" + ', '))
                 print(age)
-            except IndexError:
+            except:
                 pass
 
             ages_arr.append(age)
