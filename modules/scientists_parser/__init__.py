8e11cb7a467e28c146ff105597df7e7bcbfd243c
Sergey <s.sidoroff2011@gmail.com>
2019-12-31 08:48:28 +0300

    #31:@0h35m: added checking - parsed file is exists

--- modules/scientists_parser/__init__.py
+++ modules/scientists_parser/__init__.py
@@ -8,12 +8,22 @@ from datetime import datetime as dt
 def run():
     script_dir = os.path.dirname(__file__)
     data_path = os.path.relpath(scientists_pages_path(), script_dir) + '/data'
-    scientists_pages = os.listdir(data_path)
+    scientists_pages = [f for f in os.listdir(data_path) if re.match(r'scientists_pages', f)]
     print('Started', dt.now())
     parsed_data = []
     for science in scientists_pages:
+        print(science)
         file = open(data_path + '/' + science, 'r')
         scientist_type = next(file).strip('\n')
+        result_path = 'data/parsed_scientists_' + scientist_type.lower() + '.csv'
+
+        if os.path.isfile(result_path):
+            print('File {} already exists. Go to next.'.format(scientist_type.lower() + '.csv'))
+            df = pd.read_csv(result_path)
+            parsed_data.append(df)
+            file.close()
+            continue
+
         print(scientist_type)
         links = []
         for line in file:
@@ -21,8 +31,8 @@ def run():
 
         df = parse_all_scientists(links, scientist_type)
         df.to_csv('data/parsed_scientists_' + scientist_type.lower() + '.csv')
-        file.close()
         parsed_data.append(df)
+        file.close()
 
     df = pd.concat(parsed_data)
     print('Data was written', dt.now())
